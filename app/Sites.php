bfbed3daf91acfaa9bbbed83446789950d602b02
Austin Ginder <austinginder@gmail.com>
2020-09-09 14:57:30 -0400

    👌 IMPROVE: Handle site urls when configs are changed

--- app/Sites.php
+++ app/Sites.php
@@ -11,15 +11,17 @@ class Sites {
         global $wpdb;
         $db_prefix         = $wpdb->prefix;
         $db_prefix_primary = ( defined( 'TABLE_PREFIX' ) ? TABLE_PREFIX : $db_prefix );
+
         if ( $db_prefix_primary == "TABLE_PREFIX" ) { 
             $db_prefix_primary = $db_prefix;
         }
+
         $this->db_prefix_primary = $db_prefix_primary;
         $stacked_sites           = maybe_unserialize( $wpdb->get_results("select option_value from {$db_prefix_primary}options where option_name = 'stacked_sites'")[0]->option_value );
         if ( empty( $stacked_sites ) ) {
             $stacked_sites = [];
         }
-        $this->sites = $stacked_sites;
+        $this->sites             = $stacked_sites;
     }
 
     public function get() {
@@ -54,6 +56,15 @@ class Sites {
                     $wpdb->query("UPDATE stacked_{$site["stacked_site_id"]}_options set option_value = 'https://{$site["domain"]}' where option_name = 'siteurl'");
                     $wpdb->query("UPDATE stacked_{$site["stacked_site_id"]}_options set option_value = 'https://{$site["domain"]}' where option_name = 'home'");
                 }
+            }
+        }
+        // Turn domain mappings off when not used
+        if ( $configurations->domain_mapping == "off" ) {
+            $primary_site_url = $wpdb->get_results("SELECT option_value from {$this->db_prefix_primary}options where option_name = 'siteurl'")[0]->option_value;
+            $primary_home     = $wpdb->get_results("SELECT option_value from {$this->db_prefix_primary}options where option_name = 'home'")[0]->option_value;
+            foreach( $sites as $site ) {
+                $wpdb->query("UPDATE stacked_{$site["stacked_site_id"]}_options set option_value = '{$primary_site_url}' where option_name = 'siteurl'");
+                $wpdb->query("UPDATE stacked_{$site["stacked_site_id"]}_options set option_value = '{$primary_home}' where option_name = 'home'");
             }
         }
 
